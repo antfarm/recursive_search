47ac570ed23ca3bf95091fb4769507ee610463be
sean <sean@antfarm.de>
2016-10-12 14:10:39 +0200

    Handled edge case (empty list). Added assertions for argument types.

--- search.py
+++ search.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
 
-# search strings by prefix
+# find all strings in a list that have a given prefix
 
 def prefix_search(strings, prefix):
-    if len(strings) == 1:
+    assert(isinstance(strings, list))
+    assert(isinstance(prefix, str))
+
+    if len(strings) == 0:
+        return []
+
+    elif len(strings) == 1:
         string = strings[0]
         if string[:len(prefix)] == prefix:
             return [string]
         else:
             return []
+
     else:
         index = int(len(strings) / 2)
         return prefix_search(strings[:index], prefix) \
              + prefix_search(strings[index:], prefix)
 
 
-# use function to search phone numbers with matchong area code
+# find all phone numbers with matching area code
 
-phone_numbers = ["0223 788834", "0201 657543", "0435 287784", "4348 394947", "0201 658843"]
+phone_numbers = ["0223 788834", "0201 657543", "0435 287784", "0348 394947", "0201 658843"]
 area_codes = ["0201", "0204", "0223"]
 
 for area_code in area_codes:
